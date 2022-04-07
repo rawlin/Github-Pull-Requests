726a010deed0d9730475523e6bba648df0aabaaf
Rawlin <rawlincrasto@gmail.com>
2022-04-07 19:56:54 +0530

    Initial commit

---
+++ app/src/main/java/com/rawlin/githubpullrequests/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rawlin.githubpullrequests
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
